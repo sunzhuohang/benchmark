c477422cd60ab29d692b084672dfa4772ddf9e4d
zyh-hust <zhouyuanhui@pingcap.com>
2019-08-29 20:01:36 +0800

    zhouyuanhui@pingcap.com
    Signed-off-by: zyh-hust <zhouyuanhui@pingcap.com>

---
+++ pd_client/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+	"strconv"
+)
+
+func timetick(ch chan int) {
+	tick := time.Tick(time.Minute)
+	for {
+		select {
+		case <- tick:
+			ch <- 1
+		}
+	}
+}
+
+func main(){
+	fmt.Println("begin")
+
+	ch := make(chan int)
+
+	go timetick(ch)
+
+	for  {
+		res := <- ch
+		if res == 1 {
+			go get_region()
+		}
+	}
+}
+
+func get_region() {
+	t := time.Now().Unix()
+	name := strconv.FormatInt(t,10)
+	resp,err := http.Get("http://10.233.28.217:2379/pd/api/v1/regions")
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	filename := "./Region/"+ name +".log"
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	defer f.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	f.Write(content)
+}
